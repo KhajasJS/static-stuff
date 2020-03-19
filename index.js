179309b6b961afc79eaa273bfb62399b6633c89d
KhajasJS <57632215+KhajasJS@users.noreply.github.com>
2020-03-19 13:59:07 -0500

    Serve static files

--- index.js
+++ index.js
@@ -2,9 +2,7 @@ var express = require('express');
 var app = express();
 var serv = require('http').Server(app);
 
-app.get('/', function (req, res) {
-   res.sendFile( __dirname + "/" + "genetics.html" );
-})
+app.use(express.static('files'));
 
 serv.listen(process.env.PORT);
 console.log("Server is listening on port " + process.env.PORT);
